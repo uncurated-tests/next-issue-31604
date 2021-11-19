bc6ffdc39aaf28c3ee3850e214f0a0747cc42bce
Steven <steven@ceriously.com>
2021-11-19 18:07:37 -0500

    Fix indent

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Test() {
   return (
     <div>
-				<h1>Example SVG</h1>
-			 <Image src="/images/svg/vercel.svg" width="30" height="30" alt="App Logo"></Image>
+      <h1>Example SVG</h1>
+      <Image src="/images/svg/vercel.svg" width="30" height="30" alt="App Logo"></Image>
     </div>
   );
 }
